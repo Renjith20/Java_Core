d040cce66b6539b05eeee21dcc41b20006f1625d
Renjith <renjithrp19@gmail.com>
2024-06-04 21:01:39 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String args[]){
+
+        int arr[] = new int[4];
+         arr[0] = 89;
+         arr[1] = 8;
+         arr[2] = 4;
+         arr[3] = 78;
+
+         for (int i = 0 ; i<4 ; i++){
+            System.out.println(arr[i]);
+         }
+
+    }
+}
