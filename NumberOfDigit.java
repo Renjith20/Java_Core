0656687594295d6ddad6fc885e858141b1a234e2
Renjith <renjithrp19@gmail.com>
2024-06-18 21:24:40 +0530

    Extra

---
+++ NumberOfDigit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class NumberOfDigit {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        System.out.println("Enter the Number : ");
+        int num = sc.nextInt();
+
+        while(num!=0){
+            num = num / 10;
+            count++;
+
+        }
+
+        System.out.println("Number Of Digits : " + count);
+
+
+    }    
+}
